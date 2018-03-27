d3d928f5bb11042d51e58a380bc6b7b15f40bff7
asalcedo31 <adrisal31@gmail.com>
2018-03-27 09:28:29 -0400

    updated to run one simulation at a time to allow for better parallelization

--- smc_het_eval/run_metric_sim.R
+++ smc_het_eval/run_metric_sim.R
@@ -1,4 +1,4 @@
-source('metric_test_sim.R')
+source('~/Shad_scoring/smc_het_eval/metric_test_sim.R')
 
 n.iter=500
 
@@ -29,8 +29,8 @@ sims <- run.sim(n.iter =100,
 
 sims <- read.sim(n.iter =100,
 		C.values = 4,
-		K.u.values = 2:10,
-		K.n.values = 1:2,
+		K.u.values = 2:9,
+		K.n.values = 1:3,
 		e1.values=seq(0,0.4,by=0.05),#c(0.02,0.06,0.08,0.1),
 		e2.values= seq(0,0.4,0.05)#c(0.066,0.133,0.2))
 )
@@ -44,10 +44,10 @@ run.sim(n.iter =1,
 		e2.values= 0.2)
 
 plot.sim.batch(sims)
-p1 <- test.p1( C=3:6,K.u.values=2:10,K.n.values=1,e1.values=0.2,e2.values=0.2,metrics=c("Pearson","Pseudo.V","MCC","AUPR","X2A_metric"))
-p2 <- test.p2( C=2:6,K.u.values=2:10,K.n.values=1,e1.values=0.2,e2.values=0.2,metrics=c("Pearson","Pseudo.V","MCC","AUPR","X2A_metric"))
-p3 <- test.p3( C=4,K.u.values=2:10,K.n.values=1,e1.values=seq(0,0.4,by=0.05),e2.values=0.2,metrics=c("Pearson","Pseudo.V","MCC","AUPR","X2A_metric"))
-p4 <- test.p4( C=4,K.u.values=6,K.n.values=c(0,1,2),e1.values=0.2,e2.values=seq(0,0.4,0.05),metrics=c("Pearson","Pseudo.V","MCC","AUPR","X2A_metric"))
+p1 <- test.p1( C=3:6,K.u.values=2:9,K.n.values=3,e1.values=0.4,e2.values=0.4,metrics=c("Pseudo.V","MCC","AUPR","X2A_metric"))
+p2 <- test.p2( C=2:6,K.u.values=2:9,K.n.values=3,e1.values=0.4,e2.values=0.4,metrics=c("Pseudo.V","MCC","AUPR","X2A_metric"))
+p3 <- test.p3( C=4,K.u.values=2:9,K.n.values=3,e1.values=seq(0,0.4,by=0.05),e2.values=0.4,metrics=c("Pseudo.V","MCC","AUPR","X2A_metric"))
+p4 <- test.p4( C=4,K.u.values=6,K.n.values=c(1,2,3),e1.values=0.4,e2.values=seq(0,0.4,0.05),metrics=c("Pseudo.V","MCC","AUPR","X2A_metric"))
 
 prop.heatmap <- plot.properties.heatmap(p1,p2,p3,p4,"scoring_metric_data/sim_plots/metric_properties_heatmap.png")
 
@@ -60,30 +60,30 @@ useful.noise <- plot.sim(sims,c(6,NA,0.2,NA),metrics =c("Pearson","Pseudo.V","MC
 panel.size = 2.0
 font.y = 0.95
 font.cex = 0.8
-pdf("scoring_metric_data/sim_plots/all_sim.pdf",height=7,width=7.1)
+pdf("scoring_metric_data/sim_plots/all_sim2.pdf",height=7,width=7.1)
 grid.newpage()
 lay1 <- grid.layout(2,2,heights=unit(c(3.5,3.5),c("in","in")),widths=unit(c(3.5,3.5),c("in","in")))
 vp1 <- viewport(layout=lay1,name="top.vp",height=unit(7,"in"),width=unit(7,"in"))
 pushViewport(vp1)
 pushViewport(viewport(layout.pos.row=1,layout.pos.col=1,name="useful_prop"))
-useful.prop <- plot.sim(sims,c(NA,0,NA,0),metrics =c("Pearson","Pseudo.V","MCC","AUPR","X2A_metric"), filename="scoring_metric_data/sim_plots/useful_prop.png",param.interest="epsilon1")
+useful.prop <- plot.sim(sims,c(NA,0,NA,0),metrics =c("Pseudo.V","MCC","AUPR","X2A_metric"), filename="scoring_metric_data/sim_plots/useful_prop.png",param.interest="epsilon1")
 print(useful.prop,newpage=FALSE,panel.height=list(c(panel.size),c("inches")),position=c(0.1,0,1,0.9))
-grid.text('a.',x=unit(0.02,'npc'),y=unit(font.y-0.1,'npc'),gp=gpar(cex=font.cex,fontface="bold"))
+grid.text('a',x=unit(0.02,'npc'),y=unit(font.y-0.1,'npc'),gp=gpar(cex=font.cex,fontface="bold"))
 upViewport()
 pushViewport(viewport(layout.pos.row=1,layout.pos.col=2,name="useful_noise"))
-useful.noise <- plot.sim(sims,c(6,NA,0.2,NA),metrics =c("Pearson","Pseudo.V","MCC","AUPR","X2A_metric"), filename="scoring_metric_data/sim_plots/useful_noise.png",param.interest='epsilon2',print.legend=FALSE)
+useful.noise <- plot.sim(sims,c(6,NA,0.2,NA),metrics =c("Pseudo.V","MCC","AUPR","X2A_metric"), filename="scoring_metric_data/sim_plots/useful_noise.png",param.interest='epsilon2',print.legend=FALSE)
 print(useful.noise,newpage=FALSE,panel.height=list(c(panel.size),c("inches")),position=c(0.05,0,0.95,0.9))
-grid.text('b.',x=unit(0.02,'npc'),y=unit(font.y-0.1,'npc'),gp=gpar(cex=font.cex,fontface="bold"))
+grid.text('b',x=unit(0.02,'npc'),y=unit(font.y-0.1,'npc'),gp=gpar(cex=font.cex,fontface="bold"))
 upViewport()
 pushViewport(viewport(layout.pos.row=2,layout.pos.col=2,name="useful"))
 prop.heatmap <- plot.properties.heatmap(p1,p2,p3,p4,"scoring_metric_data/sim_plots/metric_properties_heatmap.png")
 print(prop.heatmap,newpage=FALSE,panel.height=list(c(panel.size),c("inches")),position=c(0.01,0.1,0.89,1))
-grid.text('c.',x=unit(0.02,'npc'),y=unit(font.y,'npc'),gp=gpar(cex=font.cex,fontface="bold"))
+grid.text('d',x=unit(0.02,'npc'),y=unit(font.y,'npc'),gp=gpar(cex=font.cex,fontface="bold"))
 upViewport()
 pushViewport(viewport(layout.pos.row=2,layout.pos.col=1,name="heatmap"))
-useful <- plot.sim(sims,c(NA,0,NA,0),metrics =c("Pearson","Pseudo.V","MCC","AUPR","X2A_metric"), filename="scoring_metric_data/sim_plots/useful.png",param.interest="K.u",print.legend=FALSE)
+useful <- plot.sim(sims,c(NA,0,NA,0),metrics =c("Pseudo.V","MCC","AUPR","X2A_metric"), filename="scoring_metric_data/sim_plots/useful.png",param.interest="K.u",print.legend=FALSE)
 print(useful,newpage=FALSE,panel.height=list(c(panel.size),c("inches")),position=c(0.1,0.1,1,1))
-grid.text('d.',x=unit(0.02,'npc'),y=unit(font.y,'npc'),gp=gpar(cex=font.cex,fontface="bold"))
+grid.text('c',x=unit(0.02,'npc'),y=unit(font.y,'npc'),gp=gpar(cex=font.cex,fontface="bold"))
 dev.off()
 
 
--- smc_het_eval/run_metric_sim_cmd.R
+++ smc_het_eval/run_metric_sim_cmd.R
@@ -1,19 +1,33 @@
-source('metric_test_sim.R')
+source('~/Shad_scoring/smc_het_eval/metric_test_sim.R')
+library(pryr)
 library(getopt)
+library(doParallel)
+registerDoParallel(cores=8)
+
 
 cli.params <- c(
                'C', 'c', 0, 'numeric',
 			   'Ku', 'u', 0, 'numeric',
 			   'Kn', 'n', 0, 'numeric',
-			   'e1', 'e', 0, 'numeric'
+			   'e1', 'e', 0, 'numeric',
+               'e2', 'f', 0, 'numeric'
                 );
 
 params = matrix(cli.params, ncol = 4, byrow = TRUE);
 opt <- getopt(params)
-sims <- run.sim(n.iter =25,
-        C.values = opt$C,
-        K.u.values = opt$Ku,
-        K.n.values = opt$Kn,
-        e1.values=opt$e1,#c(0.02,0.06,0.08,0.1),
-        e2.values= seq(0,0.4,0.05)#c(0.066,0.133,0.2))
+# sims <- run.sim.par(n.iter =10,
+#         C.values = opt$C,
+#         K.u.values = opt$Ku,
+#         K.n.values = opt$Kn,
+#         e1.values=opt$e1,#c(0.02,0.06,0.08,0.1),
+#         e2.values= c(0.066,0.133,0.2))
+# )
+# print("mem used before calling")
+# print(mem_used())
+sims <- run.sim.single(n.iter =100,
+        C = opt$C,
+        K.u = opt$Ku,
+        K.n = opt$Kn,
+        e1=opt$e1,#c(0.02,0.06,0.08,0.1),
+        e2= opt$e2#c(0.066,0.133,0.2))
 )
